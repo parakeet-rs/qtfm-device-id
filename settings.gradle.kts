06260d54db747fe8e38f25f55955f7af7370d361
Jixun Wu <jixun.moe@gmail.com>
2023-12-21 20:05:20 +0000

    chore: suppress IDE warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,10 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
