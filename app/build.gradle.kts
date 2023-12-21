de7a4d77ab2d62947af06a204e9d2104164acad7
Jixun Wu <jixun.moe@gmail.com>
2023-12-21 20:11:14 +0000

    build: change default apk name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        setProperty("archivesBaseName", "${applicationId}-v$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
